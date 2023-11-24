fcb00db5ba7213da6a661bcaa0b8804090cace41
helloworld9948 <134037451+helloworld9948@users.noreply.github.com>
2023-11-24 12:18:27 +0530

    Stack using pointers

--- Stack-pointers.c
+++ Stack-pointers.c
@@ -1,16 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
     int data;
-    struct node* next;
+    struct node *next;
 } *top = NULL;
 
 void push(int x)
 {
-    struct node* temp;
-    temp = (struct node*)malloc(sizeof(struct node));
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+    if (temp == NULL)
+    {
+        printf("\n\t\t Memory allocation failed");
+        return;
+    }
+
     if (top == NULL)
     {
         temp->data = x;
@@ -34,7 +40,7 @@ void pop()
     }
     else
     {
-        struct node* temp;
+        struct node *temp;
         printf("\n\t\t Popped element: %d", top->data);
         temp = top;
         top = top->next;
@@ -49,13 +55,34 @@ void traversal()
         printf("\n\t\t Stack is underflow");
         return;
     }
-    struct node* pi = top;
+    struct node *pi = top;
     printf("\n\t\t Stack elements: ");
     while (pi != NULL)
     {
         printf("%d ", pi->data);
         pi = pi->next;
     }
+}
+
+int isEmpty()
+{
+    return top == NULL;
+}
+
+int isFull()
+{
+
+    return 0;
+}
+
+int getTop()
+{
+    if (top == NULL)
+    {
+        printf("\n\t\t Stack is empty");
+        return -1; 
+    }
+    return top->data;
 }
 
 int main()
@@ -66,7 +93,10 @@ int main()
     printf("\n\t\t Push -> 1");
     printf("\n\t\t Pop  -> 2");
     printf("\n\t\t Traversal -> 3");
-    printf("\n\t\t Exit -> 4");
+    printf("\n\t\t Is Empty -> 4");
+    printf("\n\t\t Is Full  -> 5");
+    printf("\n\t\t Get Top  -> 6");
+    printf("\n\t\t Exit -> 7");
     printf("\n\t-------------------------------\n");
     while (1)
     {
@@ -86,6 +116,15 @@ int main()
             traversal();
             break;
         case 4:
+            printf("\n\t\t Is Empty: %s", isEmpty() ? "Yes" : "No");
+            break;
+        case 5:
+            printf("\n\t\t Is Full: %s", isFull() ? "Yes" : "No");
+            break;
+        case 6:
+            printf("\n\t\t Top element: %d", getTop());
+            break;
+        case 7:
             return 0;
         default:
             printf("\n\t\t Invalid choice --\n");
